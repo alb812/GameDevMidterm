3e38915bd8e239a7f654f8bdb97cee7698697c74
Ariana B <alb886@nyu.edu>
2018-10-09 12:29:38 -0400

    Timer, MainMenu, and Other Scenes

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Intent: Activates buttons on ManiMenu Scene
+//Usage: put on MainMenu gameObject
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    
+    public void InfoScene()
+    {
+        SceneManager.LoadScene("InfoScene");
+    }
+    
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("StartGame");
+    }
+    
+}
+
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+//Usage: 
+//Intent: This script is a timer that leads to a GameOver when time runs out
+public class TimerScript : MonoBehaviour
+{
+	//defining basic variables
+	
+	//defines timer UI
+	public Text timerText;
+	//defines how many seconds are there
+	public int timeLeft = 120;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//using a coroutine so the timer runs until completely up
+		StartCoroutine("CountDown");
+		
+		//setting timer seconds
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//displaying the timer in the UI
+		timerText.text = "Time Left = " + timeLeft;
+		
+		//If timer goes less than 0, stop timer and load game over scene
+		if (timeLeft <= 0)
+		{
+			StopCoroutine("CountDown");
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+	
+	//Timer goes down each second
+	IEnumerator CountDown()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1);
+			timeLeft--;
+		}
+	}
+}
